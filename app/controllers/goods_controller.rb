5c621c17997bd4719ab7002c48d45fea301c448d
OnishiNaoki <balance22@gmai.com>
2018-03-06 20:03:05 +0900

    onishi

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,21 +1,16 @@
 class GoodsController < ApplicationController
   def create
 
-    pp 'create'
-    pp params
-    
+    @plan = Plan.find(params[:plan_id])
     @good = Good.create(user_id: current_user.id, plan_id: params[:plan_id])
     @goods = Good.where(plan_id: params[:plan_id])
-    puts params[:plan_id]
   end
 
   def destroy
-    
-    pp 'destory'
-pp params
 
-    good = Good.find_by(user_id: current_user.id, plan_id: params[:plan_id])
-    good.destroy
+    @plan = Plan.find(params[:plan_id])
+    @good = Good.find_by(user_id: current_user.id, plan_id: params[:plan_id])
+    @good.destroy
     @goods = Good.where(plan_id: params[:plan_id])
   end
 end
